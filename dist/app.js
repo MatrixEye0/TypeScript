bdd5e7d0d1196e812962713f245be670c002af47
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-13 19:24:06 +0530

    Learn TypeScript

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let test = 15;
+console.log(test);
+let test2 = "Jagjit";
+console.log(test2);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+let test : number = 15;
+console.log(test);
+let test2:string ="Jagjit";
+console.log(test2);
